8565c8c86e410a0c254430d018f2b60ddafc5a30
pacoan <pacoandresh@gmail.com>
2017-11-08 10:41:06 +0100

    picamnuevo __errores

---
+++ developing/client_alphabot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#____________developed by paco andres____________________
+
+import threading
+from nodeproxy import clientNODERB
+import time
+import Pyro4
+import cv2
+import urllib
+import numpy as np
+
+def run_camera(cam):
+    while True:
+        #c=np.fromstring(cam.image,dtype=np.uint8)
+        c = cv2.imdecode(np.fromstring(cam.image, dtype=np.uint8), -1)
+        c.resize(320,240)
+        #ret,jpeg=cv2.imencode('jpg',c)
+
+        cv2.imshow('alphabot',c)
+        if cv2.waitKey(1) == 27:
+            exit(0)
+
+bot=clientNODERB("alphabot@158.49.241.45")
+cam=threading.Thread(target=run_camera,args=(bot.camera,))
+cam.setDaemon(True)
+cam.start()
+
+time.sleep(2)
+while True:
+  pass
---
+++ developing/nodeRBB/SERVICES/picamnuevo.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#lock().acquire()
+#____________developed by paco andres____________________
+import time
+import datetime
+from nodeRBB.LIBS import control
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import cv2
+import Pyro4
+from io import BytesIO
+
+@Pyro4.expose
+class picamnuevo(control.control):
+    @control.loadconfig
+    def __init__(self,data,**kwargs):
+        self.camera =PiCamera()
+        self.camera.resolution=(self.width,self.height)
+        self.camera.framerate = 24
+        #self.camera.sharpness = 0
+        self.camera.contrast = 0
+        self.camera.brightness = 50
+        #self.camera.saturation = 0
+        #self.camera.ISO = 0
+        self.camera.video_stabilization = True
+        self.camera.exposure_compensation = 0
+        #self.camera.exposure_mode = 'auto'
+        #self.camera.meter_mode = 'average'
+        #self.camera.awb_mode = 'auto'
+        self.camera.image_effect = 'none'
+        self.camera.color_effects = None
+        self.camera.rotation = 0
+        self.camera.hflip = True
+        self.camera.vflip = False
+        self.buffer=[BytesIO(),BytesIO()]
+        self.available=0
+        self.lock=1
+        super(picamnuevo,self).__init__((self.worker_read,))
+
+    def worker_read(self):
+
+            while self.worker_run:
+                self.buffer[self.lock].truncate()
+                self.camera.capture_continuous(self.buffer[self.lock], "jpeg",use_video_port=True)
+                self.lock,self.available=self.available,self.lock
+                time.sleep(self.frec)
+
+    def worker_publ(self):
+        while self.worker_run:
+            try:
+              for k,v in self.subscriptors.iteritems():
+                  v.publication(self.buffer[self.available][k])
+            except:
+              #print("cam dist error")
+              pass
+    @property
+    def image(self):
+        #print ("cam %s" % (self.available))
+        self.buffer[self.available].seek(0)
+        #print size(self.buffer[self.available].read())
+        return self.buffer[self.available].read()
+
+    def subscribe(self,key,uri):
+        try:
+          self.subscriptors[key]=Pyro4.Proxy(uri)
+          print ("arduino subcriptor %s %s" %(key,uri))
+          return True
+        except:
+          return False
+
+if __name__ == "__main__":
+    pass
