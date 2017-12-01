49f5880ad5a585ba8627d0b2d4de0e94de875c89
Lonami Exo <totufals@hotmail.com>
2017-12-01 12:40:33 +0100

    Make module loader more reusable, and load sensors

--- developing/node/importer.py
+++ developing/node/importer.py
@@ -3,27 +3,33 @@ import inspect
 import importlib
 
 
-# We're going to work with absolute imports as it's just easier.
-# We'll get something like "node.importer", we want "node.our_thing".
-SERVICES_PKG = '.'.join(__name__.split('.')[:-1]) + '.services'
-SERVICES_DIR = os.path.abspath(os.path.join(__file__, os.pardir, 'services'))
-
-
 _classes = {}
+MODULES = ('services', 'sensors')
 
 
-print('Populating dictionary of available services...')
-for f in os.listdir(SERVICES_DIR):
-    name, ext = os.path.splitext(f)
-    if ext == '.py' and name != '__init__':
-        try:
-            module = importlib.import_module(SERVICES_PKG + '.' + name)
-            for name, obj in inspect.getmembers(module):
-                if inspect.isclass(obj):
-                    _classes[name] = obj
-                    print('Added class "%s"' % name)
-        except ImportError as e:
-            print('Loading module "%s" failed due to %s' % (name, str(e)))
+def load_module(module):
+    # We're going to work with absolute imports as it's just easier.
+    # We'll get something like "node.importer", we want "node.our_thing".
+    mod_pkg = '.'.join(__name__.split('.')[:-1]) + '.' + module
+    mod_dir = os.path.abspath(os.path.join(__file__, os.pardir, module))
+
+    print 'Populating dictionary of available classes in %s...' % module
+    for f in os.listdir(mod_dir):
+        name, ext = os.path.splitext(f)
+        if ext == '.py' and name != '__init__':
+            try:
+                mod = importlib.import_module(mod_pkg + '.' + name)
+                for name, obj in inspect.getmembers(mod):
+                    if inspect.isclass(obj):
+                        _classes[module + '.' + name] = obj
+                        print 'Added class "%s"' % name
+
+            except ImportError as e:
+                print 'Loading module "%s" failed due to %s' % (name, str(e))
+
+
+for module in MODULES:
+    load_module(module)
 
 
 def get_class(name):
