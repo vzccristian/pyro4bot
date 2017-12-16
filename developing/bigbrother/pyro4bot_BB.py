660ade0a958b9b3bdd8f5edbeaa71dcf7be55f68
Cristian Vazquez <vzccristian@gmail.com>
2017-12-16 16:26:48 +0100

    Working on bigbrother

--- developing/bigbrother/pyro4bot_BB.py
+++ developing/bigbrother/pyro4bot_BB.py
@@ -2,34 +2,78 @@ import Pyro4
 import Pyro4.naming as nm
 import utils
 import sys
+import threading
+from termcolor import colored
+import time
 
 INTERFACE = "wlan0"
 
+
+def printThread(color="green"):
+    return ((colored("[" + threading.current_thread().getName() + "]", color)))
+
+
 class bigbrother (object):
-    def __init__(self,ethernet="wlan0"):
+    def __init__(self, ethernet="wlan0"):
         if (ethernet is not "wlan0"):
             INTERFACE = ethernet
+        self.ns = ""  # Thread
+        self.nameServerWorking = False
+        self.nameserver = ""
         self.start()
 
     def start(self):
-        print "Checking name_server"
+        print printThread(color="red"), "--> Checking name_server"
         try:
             # Working
             ns = Pyro4.locateNS()
         except:
-            self.createNameServer()
-            # Not working
+            self.nameServerWorking = True
+            self.ns = threading.Thread(target=self.createNameServer, args=())
+            self.ns.start()
+
 
     def createNameServer(self):
-        print "Creating new name server on:", INTERFACE
+        print printThread(), "--> Creating new name server on:", colored(INTERFACE, "magenta")
         try:
-            nm.startNS(host=utils.get_ip_address(INTERFACE))
+            self.nameserver=nm.startNSloop(host=utils.get_ip_address(INTERFACE))
         except:
             raise
 
+    def test(self):
+        print "test"
+
+    def list(self):
+        print "List Nameserver"
+        # TODO
+        # self.nameserver.nm.NameServer.list()
+
+
+    def execute(self,command):
+        return {
+            'list': self.list,
+            'test' : self.test,
+        }.get(command,"Error")
+
 
 if __name__ == "__main__":
+    threading.current_thread().setName("Main")
     if len(sys.argv) > 1:
-        print sys.argv[1]
         INTERFACE = sys.argv[1]
     bb = bigbrother(ethernet=INTERFACE)
+
+    while (bb.nameServerWorking):
+        time.sleep(1)
+        command = raw_input("\n"+colored("Comando a la espera: ","yellow"))
+        try:
+            realCommand=bb.execute(command)
+            if (realCommand is not "Error"):
+                realCommand()
+        except:
+            raise
+            print (colored("Comando erroneo","red"))
+
+    if (bb.nameServerWorking):
+        bb.ns.join()
+
+    print printThread(color="red"), "Saliendo..."
