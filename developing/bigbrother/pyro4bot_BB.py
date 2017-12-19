b86d575f36251d13f97f0b1bcdf5050360000046
vzccristian <cvazquezu@alumnos.unex.es>
2017-12-19 12:23:55 +0100

    Big-brother v0.0.1

--- developing/bigbrother/pyro4bot_BB.py
+++ developing/bigbrother/pyro4bot_BB.py
@@ -13,24 +13,29 @@ class bigbrother (object):
     def __init__(self, ethernet="wlan0"):
         if (ethernet is not "wlan0"):
             INTERFACE = ethernet
-        self.ns = ""  # Thread
+        self.ns = None #NS
+        self.nsThread = None  # Thread
         self.nameServerWorking = False
         self.ready = False
         self.nameserver = ""
         self.start()
 
     def start(self):
+        # Main thread
         print printT(color="yellow"), "--> Checking name_server"
         try:
             # Working
             ns = Pyro4.locateNS()
             print "NameServer already working"
         except:
+            # Not working
             self.nameServerWorking = True
-            self.ns = threading.Thread(target=self.createNameServer, args=())
-            self.ns.start()
+            self.nsThread = threading.Thread(target=self.createNameServer, args=())
+            self.nsThread.start() #Thread-1 started
+            self.ns = Pyro4.locateNS() #Getting NS on main thread
 
     def createNameServer(self):
+        # Thread-1
         global INTERFACE
         print printT(), "--> Creating new name server on:", colored(INTERFACE, "magenta")
         ip="localhost"
@@ -40,23 +45,46 @@ class bigbrother (object):
                 print "Error al seleccionar interfaz. "
                 INTERFACE = raw_input("\n"+colored("Introducir interfaz de red a utilizar: ","yellow"))
         try:
-            print printT(),"ready ---> true"
             self.ready = True
-            self.nameserver=nm.startNSloop(host=ip)
+            self.nameserver=nm.startNSloop(host=ip) #NS ready
         except:
             print "Error al crear el nameserver"
 
-    def test(self):
-        print "test"
-
     def list(self):
-        print "List Nameserver"
-        # self.nameserver.nm.NameServer.list()
+        try:
+            print "------------------ NAME-SERVER LIST ------------------"
+            print self.ns.list()
+        except:
+            print "Error name-server"
+            raise
+
+    def listprefix(self, _prefix):
+        entries=self.ns.list(prefix=_prefix)
+        return entries
+    def listregex(self,_regex):
+        entries=self.ns.list(regex=_regex)
+        return entries
+
+    def lookup(self,name):
+        try:
+            uri=self.ns.lookup(name)
+            print "Looking for "+name+" : "+colored(uri,"green")
+            return uri
+        except Pyro4.errors.NamingError:
+            print name + " no encontrado."
+
+    def remove(self,name):
+        try:
+            print "Removed "+name+" : "+colored(self.ns.lookup(name),"red")
+            self.ns.remove(name)
+        except Pyro4.errors.NamingError:
+            print name + " no encontrado."
 
     def execute(self,command):
         return {
             'list': self.list,
-            'test' : self.test,
+            'remove' : self.remove,
+            'lookup' : self.lookup,
         }.get(command,"Error")
 
 
@@ -66,14 +94,23 @@ if __name__ == "__main__":
         INTERFACE = sys.argv[1]
     bb = bigbrother(ethernet=INTERFACE)
 
+    #Main thread
     while (bb.nameServerWorking):
         time.sleep(1)
         if (bb.ready):
             command = raw_input("\n"+colored("Comando a la espera: ","yellow"))
             try:
-                realCommand=bb.execute(command)
+                command = command.split()
+                realCommand=bb.execute(command[0])
                 if (realCommand is not "Error"):
-                    realCommand()
+                    if (len(command) is 1):
+                        realCommand()
+                    elif (len(command) is 2):
+                        uri = realCommand(command[1])
+                        obj = Pyro4.Proxy(uri)
+                        # obj.method()
+                else:
+                    print "Comando no encontrado."
             except:
                 raise
                 print (colored("Comando erroneo","red"))
