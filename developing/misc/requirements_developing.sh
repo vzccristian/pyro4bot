693a1f40640040fd14c507d273bd27bfbcead52f
jmagundezg <jmagundezg@gmail.com>
2018-02-01 18:03:35 +0100

    revise scripts

--- developing/misc/requirements_developing.sh
+++ developing/misc/requirements_developing.sh
@@ -1,18 +1,18 @@
-sudo add-apt-repository ppa:webupd8team/atom
-sudo apt update
-sudo apt install python-pip
-sudo apt install atom
-sudo apt install python-opencv
-apm install teletype
-pip2.7 install -r "requirements.txt"
-sudo apt install git
+# sudo add-apt-repository ppa:webupd8team/atom
+# sudo apt update
+# sudo apt install python-pip
+# sudo apt install atom
+# sudo apt install python-opencv
+# apm install teletype
+# pip2.7 install -r "requirements.txt"
+# sudo apt install git
 Yellow='\033[0;33m'
-echo "${Yellow}Quieres instalar Spotify? [y/n]"
+echo -e "${Yellow}Quieres instalar Spotify? [y/n]"
 read val
 if [ val="y" ]
 then
- snap install spotify
-  echo "${Yellow} spotify instalado"
+# snap install spotify
+  echo -e "${Yellow} spotify instalado"
 else
-    echo "${Yellow} xd"
+    echo -e "${Yellow} xd"
   fi
