b55eb10d0ca81f906204a0a73b0c9f4c8556519a
Alberto <albertoserranoleon@gmail.com>
2019-01-24 13:25:56 +0100

    Changed import

--- developing/install_packages.py
+++ developing/install_packages.py
@@ -4,15 +4,15 @@
 # _________collaboration with cristian vazquez____________
 # All datas defined in json configuration are atributes in your code object
 """
-This module find in node all paCkages with ImportError and try to INSTALL
+This module find in node all packages with ImportError and try to INSTALL
 with pip module.
 """
 import pip
-from node.libs.inspection import _modules_libs_errors, not_finded_modules
+from node.libs.inspection import _modules_libs_errors, not_found_modules
 
 print("# PYRO4BOT #")
 print("MODULES TO INSTALL:")
-modulestointall = not_finded_modules(_modules_libs_errors)
+modulestointall = not_found_modules(_modules_libs_errors)
 for mod in modulestointall:
     print("\t{}".format(mod))
 if not modulestointall:
